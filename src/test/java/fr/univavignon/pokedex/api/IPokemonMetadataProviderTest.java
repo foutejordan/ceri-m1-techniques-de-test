9c3ae23f42073c8838f09e0384d5fd6572accf84
foutejordan <foutetapejordan10@gmail.com>
2023-04-19 10:17:54 +0200

    merge

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -11,14 +11,13 @@ public class IPokemonMetadataProviderTest {
         IPokemonMetadataProvider metaDataMock = mock(IPokemonMetadataProvider.class);
         when(metaDataMock.getPokemonMetadata(0)).thenReturn(new PokemonMetadata(0, "Bulbizarre", 126, 126,90));
 
-        //Pokemon pk1 = new Pokemon(0, "Bulbizarre", 126, 126,90,613,64,4000,4,56);
+        Pokemon pk1 = new Pokemon(0, "Bulbizarre", 126, 126,90,613,64,4000,4,56);
 
-        //Pokemon pk2 = new Pokemon(133, "Aquali",186,168,260,2729,202,5000,4,100);
+        Pokemon pk2 = new Pokemon(133, "Aquali",186,168,260,2729,202,5000,4,100);
 
         PokemonMetadata result = metaDataMock.getPokemonMetadata(0);
 
         assertEquals(new PokemonMetadata(0, "Bulbizarre", 126, 126,90), result);
-
         verify(metaDataMock).getPokemonMetadata(0);
 
     }
