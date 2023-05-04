fd74ac46feb0b6781865be8568225085ad9b4e48
ftjordan <foutetapejordan10@gmail.com>
2023-05-04 07:58:42 +0200

    debut TP6

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -17,7 +17,8 @@ public class IPokemonFactoryTest {
 
     private static Pokemon pokemon1, pokemon2;
 
-    private static IPokemonFactory pokemonFactory;
+    //private static IPokemonFactory pokemonFactory;
+    private static RocketPokemonFactory pokemonFactory;
 
 
 
@@ -28,7 +29,7 @@ public class IPokemonFactoryTest {
         Mockito.when(metadataProvider.getPokemonMetadata(0)).thenReturn(bulbasaurMetadata);
         Mockito.when(pokemonFactory.createPokemon(0, 613, 64, 4000, 4)).thenReturn(new Pokemon(0, "Bulbizarre", 126, 126, 90,613, 64, 4000, 4,56));*/
 
-        pokemonFactory = new PokemonFactory();
+        pokemonFactory = new RocketPokemonFactory();
         pokemon1 = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56);
         pokemon2 = new Pokemon(133, "Aquali", 186, 168, 260, 2729, 202, 5000, 4, 100);
     }
@@ -36,22 +37,22 @@ public class IPokemonFactoryTest {
     @Test
     public void testCreatePokemon() throws PokedexException {
         Pokemon newPokemon = pokemonFactory.createPokemon(0, 613, 64, 4000, 4);
-        Assert.assertEquals(newPokemon.getCp(), pokemon1.getCp());
-        Assert.assertEquals(newPokemon.getHp(), pokemon1.getHp());
-        Assert.assertEquals(newPokemon.getDust(), pokemon1.getDust());
-        Assert.assertEquals(newPokemon.getCandy(), pokemon1.getCandy());
+        assertEquals(newPokemon.getCp(), pokemon1.getCp());
+        assertEquals(newPokemon.getHp(), pokemon1.getHp());
+        assertEquals(newPokemon.getDust(), pokemon1.getDust());
+        assertEquals(newPokemon.getCandy(), pokemon1.getCandy());
     }
     @Test
     public void testCreatePokemon2() throws PokedexException {
         //when(pokemonFactory.createPokemon(133,2729,202,5000,4)).thenReturn(aquali);
         Pokemon newPokemon = pokemonFactory.createPokemon(133,2729,202,5000,4);
 
-        Assert.assertEquals(newPokemon.getCp(), pokemon2.getCp());
-        Assert.assertEquals(newPokemon.getCandy(), pokemon2.getCandy());
-        Assert.assertEquals(newPokemon.getDust(), pokemon2.getDust());
-        Assert.assertEquals(newPokemon.getHp(), pokemon2.getHp());
-        Assert.assertEquals(newPokemon.getIndex(), pokemon2.getIndex());
-        Assert.assertEquals(newPokemon.getAttack(), pokemon2.getAttack());
+        assertEquals(newPokemon.getCp(), pokemon2.getCp());
+        assertEquals(newPokemon.getCandy(), pokemon2.getCandy());
+        assertEquals(newPokemon.getDust(), pokemon2.getDust());
+        assertEquals(newPokemon.getHp(), pokemon2.getHp());
+        assertEquals(newPokemon.getIndex(), pokemon2.getIndex());
+        assertEquals(newPokemon.getAttack(), pokemon2.getAttack());
         Assert.assertTrue(newPokemon.getIv() >=0 && newPokemon.getIv() <=100);
     }
 }
